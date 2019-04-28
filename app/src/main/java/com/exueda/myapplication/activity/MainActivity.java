9cf06ca201350f61ee25f7ef2785a4d9bfa7395d
YouK <2072203001@qq.com>
2019-04-28 13:33:53 +0800

    测试ss

--- app/src/main/java/com/exueda/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/exueda/myapplication/activity/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private Context context;
 
+    private String ss;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
